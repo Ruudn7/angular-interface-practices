3258f7a40dc58c89c4f973cdea0226e5a011022a
Sławomir <ruudn7@wp.pl>
2021-01-15 23:29:15 +0100

    add open and hid mmessage functionality

--- src/app/ui/static-card/static-card.component.ts
+++ src/app/ui/static-card/static-card.component.ts
@@ -1,19 +1,24 @@
-import { Attribute, Component, Input, OnInit } from '@angular/core';
+import { Attribute, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-static-card',
   templateUrl: './static-card.component.html',
   styleUrls: ['./static-card.component.scss']
 })
-export class StaticCardComponent implements OnInit {
+export class StaticCardComponent {
 
+  @Input() open = false;
   @Input() text = '';
+
+  @Output() closeEvent: EventEmitter<boolean> = new EventEmitter();
 
   constructor(
     @Attribute('type') public type: string
   ) { }
 
-  ngOnInit(): void {
+  close(): void {
+    this.open = false;
+    this.closeEvent.emit(this.open);
   }
 
 }
